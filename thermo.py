18f8bef9d83ccb8c878a86f7cd78e6c721db72f7
mcmspark <mcmspark@hotmail.com>
2016-05-31 09:09:58 -0400

    Update thermo.py
    Now reads temp from sensor network and accuweather and logs to file

--- thermo.py
+++ thermo.py
@@ -4,24 +4,27 @@
 import kivy
 from kivy.app import App
 from kivy.utils import get_color_from_hex as rgb
+from kivy.clock import Clock
 from kivy.uix.image import Image
 from kivy.graphics import Color, Rectangle
 from kivy.uix.label import Label
-#import serial
-from time import time, sleep, gmtime, strftime
+import serial
+from time import time, sleep, gmtime, strftime, localtime
 import urllib
 import re
 
 class Thermo(App):
 	DEVICE = '/dev/ttyAMA0'
 	BAUD = 9600
-	TIMEOUT = 30
+	TIMEOUT = 5
+	lastWeatherRead=0.0
+	updated=False
 	locs = ['master','tess','hall','lr','window']
-	devices = ['AA','AB','AC','AD','AE']
-	temps = [0.0,0.0,0.0,0.0,0.0]
-	batts = [0.0,0.0,0.0,0.0,0.0]
-	times = [0.0,0.0,0.0,0.0,0.0]
-	ser = []#serial.Serial(DEVICE, BAUD)
+	devices = ['AA','AB','AC','AD','AE','AW']
+	temps = [0.0,0.0,0.0,0.0,0.0,0.0]
+	batts = [0.0,0.0,0.0,0.0,0.0,0.0]
+	times = [0.0,0.0,0.0,0.0,0.0,0.0]
+	ser = serial.Serial(DEVICE, BAUD)
 	voltage = 0.0
 	tempvale = 0.0
 	setpt = []
@@ -30,9 +33,10 @@ class Thermo(App):
 		self.setpt = Label(font_size=44, markup=True, text="hello", pos=(55,55))
 		with picture.canvas:
 			Color(1,0,0,0.5)
-			Rectangle(pos=(50, 50), size=(200,200))
+			Rectangle(pos=(50, 75), size=(270,175))
 
 		picture.add_widget(self.setpt)
+		Clock.schedule_interval(self.mainLoop, 10.0)
 		return picture
 
 	def loadConfig(self):
@@ -56,40 +60,45 @@ class Thermo(App):
 		voltage = 0
 		tempvalue = -100
 		deviceid = ''
-		return
 		while (time()<fim) and (tempvalue == -100):
-			n = 0 #ser.inWaiting()
+			n = self.ser.inWaiting()
 			if n != 0:
-				data = ser.read(n)
+				data = self.ser.read(n)
 				nb_msg = len(data) / 12
 				for i in range (0, nb_msg):
 					msg = data[i*12:(i+1)*12]
 		
 					deviceid = msg[1:3]
-					if not( deviceid in devices ):
-						devices.append(deviceid)
+					if not( deviceid in self.devices ):
+						self.devices.append(deviceid)
 		
 					if msg[3:7] == "TEMP":
 						tempvalue = msg[7:]
-					temps[devices.index(deviceid)]=tempvalue
-			
+						self.temps[self.devices.index(deviceid)]=tempvalue
+						self.updated=True
+
 					if msg[3:7] == "BATT":
 						voltage = msg[7:11]
 						if voltage == "LOW":
 							voltage = 0
-						batts[devices.index(deviceid)]=voltage
+						self.batts[self.devices.index(deviceid)]=voltage
 			else:
 				sleep(5)
 		return
 
 	def getWeather(self):
-		# get and parse AccuWeather data
-		cur = re.compile('Currently: (.*)<')
-		link = "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=US|44022"
-		f = urllib.urlopen(link)
-		myfile = f.read()
-		print self.setpt.text
-		self.setpt.text = cur.search(myfile).group(1)
+		if(time()-self.lastWeatherRead > 1800):
+			# get and parse AccuWeather data
+			cur = re.compile('Currently: (.*)<')
+			link = "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=US|44022"
+			f = urllib.urlopen(link)
+			myfile = f.read()
+			tempvalue = cur.search(myfile).group(1)
+
+			self.temps[self.devices.index('AW')]=tempvalue
+			self.updated=True		
+			self.setpt.text = tempvalue
+			self.lastWeatherRead = time()
 		return
 	
 	def uploadData(self):
@@ -97,9 +106,10 @@ class Thermo(App):
 		# add it to the memory deque
 		# if the deque > 10 try to upload it and any pending updates
 		# else throw a flag for pending updates and write to a file
-		if tempvalue > -100:
+		if self.updated:
+			self.updated=False
 			with open("Output.txt", "a") as text_file:
-				text_file.write("{},{},{}\r\n".format(time(),temps, batts))
+				text_file.write("{},{},{}\r\n".format(strftime('%a, %d %b %Y %H:%M:%S',localtime()),self.temps, self.batts))
 		return
 		
 	def downloadRequests(self):
@@ -110,18 +120,16 @@ class Thermo(App):
 		# contains all rules to make decisions based on data 
 		return
 	
-	def mainLoop(self):
+	def mainLoop(self,args):
 		self.loadConfig()
-		#while(1):
-		
 		self.getWeather()
 		self.readSensors()
 		self.updateDisplay()
 		self.uploadData()
 		self.downloadRequests()
 		self.takeAction()
-		sleep(5)
 	
 if __name__ == '__main__':
 	Thermo().run()
-	Thermo().mainLoop()
+
+
